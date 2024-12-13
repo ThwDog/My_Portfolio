3e5c0eaec3778bfcd880e8c043c5fc2ae741fa5b
ThwDog <thanadul.sod.work@gmail.com>
2024-12-13 13:24:32 +0700

    Hot Fix

--- src/Function/NavBar.jsx
+++ src/Function/NavBar.jsx
@@ -1,5 +1,6 @@
 import './NavBar.css';
 import React, { useEffect, useState ,useRef } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -23,10 +24,10 @@ export default function NavBar() {
         <>
         <nav className={`nav ${isScrolled ? 'scrolled' : ''}`}>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="#/AboutMe" onClick={() => aboutMeRef.current.scrollIntoView({ behavior: 'smooth' })}>About me</a></li>
-              <li><a href="#/MyWork" onClick={() => myWorkRef.current.scrollIntoView({ behavior: 'smooth' })}>My work</a></li>
-              <li><a href="#/Contract" onClick={() => contactRef.current.scrollIntoView({ behavior: 'smooth' })}>Contact</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/AboutMe" onClick={() => aboutMeRef.current.scrollIntoView({ behavior: 'smooth' })}>About me</Link></li>
+              <li><Link to="/MyWork" onClick={() => myWorkRef.current.scrollIntoView({ behavior: 'smooth' })}>My work</Link></li>
+              <li><Link to="/Contract" onClick={() => contactRef.current.scrollIntoView({ behavior: 'smooth' })}>Contact</Link></li>
             </ul>
         </nav>
         </>
